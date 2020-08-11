101c02edac036b6d80dddf27b8863485c3b3ca44
faridrakh <faridrakh@yahoo.com>
2020-08-11 20:38:49 +0700

    test

--- src/main/java/com/brs/project/ProjectApplication.java
+++ src/main/java/com/brs/project/ProjectApplication.java
@@ -8,5 +8,13 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+		
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
 	}
 }
